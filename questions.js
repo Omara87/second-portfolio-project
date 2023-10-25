6348609a86e81f139d28c8b9d85d1e25ec3e227f
Omara87 <>
2023-10-25 12:37:15 +0000

    Added constant variables for JS

---
+++ questions.js
@@ -0,0 +1,253 @@
+// questions that will be asked
+
+var questions = [{
+    question: "Where did Hagrid get the dragon egg?",
+    answers: {
+        choice: 'Diagon Alley',
+        choice: 'Knockturn Alley',
+        choice: 'Hogs Head',
+        choice: 'Leaky Cauldron',
+    },
+    correctAnswer: 3,
+},
+{
+    question: "How many knuts are there to a sickle?",
+    answers: {
+        choice: '16',
+        choice: '29',
+        choice: '21',
+        choice: '13',
+    },
+    correctAnswer: 2,
+},
+{
+    question: "What is rumoured to guard some of the high security Gringotts vaults?",
+    answers: {
+        choice: 'Dragons',
+        choice: 'Trolls',
+        choice: 'Dementors',
+        choice: 'Giants',
+    },
+    correctAnswer: 1,
+},
+{
+    question: "Who is the ghost of Hufflepuff?",
+    answers: {
+        choice: 'Bloody Baron',
+        choice: 'Grey Lady',
+        choice: 'Fat Friar',
+        choice: 'Nearly Headless Nick',
+    },
+    correctAnswer: 3,
+},
+{
+    question: "What animal do spiders most fear?",
+    answers: {
+        choice: 'Hippogriff',
+        choice: 'Thestral',
+        choice: 'Chimera',
+        choice: 'Basilisk',
+    },
+    correctAnswer: 4,
+},
+{
+    question: "What spell lights up the tip of your wand?",
+    answers: {
+        choice: 'Alohamora',
+        choice: 'Lumos',
+        choice: 'Reducto',
+        choice: 'Expelliarmus',
+    },
+    correctAnswer: 2,
+},
+{
+    question: "What animal represents Ravenclaw house?",
+    answers: {
+        choice: 'Lion',
+        choice: 'Snake',
+        choice: 'Eagle',
+        choice: 'Badger',
+    },
+    correctAnswer: 3,
+},
+{
+    question: "What colour is the Weasley's flying car?",
+    answers: {
+        choice: 'Blue',
+        choice: 'Green',
+        choice: 'Black',
+        choice: 'Grey',
+    },
+    correctAnswer: 1,
+},
+{
+    question: "Who owns the ice-cream shop in Diagon Alley?",
+    answers: {
+        choice: 'Ollivanders',
+        choice: 'Flourish and Blott',
+        choice: 'Madam Malkins',
+        choice: 'Florean Fortescue',
+    },
+    correctAnswer: 4,
+},
+{
+    question: "Where do the Weasleys go on holiday in the Prisoner of Azkaban?",
+    answers: {
+        choice: 'France',
+        choice: 'Greece',
+        choice: 'Egypt',
+        choice: 'Romania',
+    },
+    correctAnswer: 3,
+},
+{
+    question: "What is the name of Hermione's cat?",
+    answers: {
+        choice: 'Errol',
+        choice: 'Crookshanks',
+        choice: 'Hedwig',
+        choice: 'Scabbers',
+    },
+    correctAnswer: 2,
+},
+{
+    question: "What do the dementors take when they kiss you?",
+    answers: {
+        choice: 'Magic ability',
+        choice: 'Happiness',
+        choice: 'Soul',
+        choice: 'Life',
+    },
+    correctAnswer: 3,
+},
+{
+    question: "What was the nickname of James Potter?",
+    answers: {
+        choice: 'Prongs',
+        choice: 'Moony',
+        choice: 'Wormtail',
+        choice: 'Padfoot',
+    },
+    correctAnswer: 1,
+},
+{
+    question: "What spell allows things to be concealed?",
+    answers: {
+        choice: 'Patronus Charm',
+        choice: 'Fidelius Charm',
+        choice: 'Caterwauling Charm',
+        choice: 'Imperterbable Charm',
+    },
+    correctAnswer: 2,
+},
+{
+    question: "Which teams played in the Quidditch World Cup?",
+    answers: {
+        choice: 'USA and Hungary',
+        choice: 'France and Norway',
+        choice: 'England and Romania',
+        choice: 'Ireland and Bulgaria',
+    },
+    correctAnswer: 4,
+},
+{
+    question: "What is the name of the magical school in France?",
+    answers: {
+        choice: 'Beauxbatons',
+        choice: 'Durmstrang',
+        choice: 'Ilvermorny',
+        choice: 'Mahoutokoro',
+    },
+    correctAnswer: 1,
+},
+{
+    question: "What does Harry Potter use in the second task in The Goblet of Fire?",
+    answers: {
+        choice: 'Confundus Charm',
+        choice: 'Gillyweed',
+        choice: 'Bubble-head Charm',
+        choice: 'Transfiguration',
+    },
+    correctAnswer: 2,
+},
+{
+    question: "What does Professor Binns teach?",
+    answers: {
+        choice: 'Charms',
+        choice: 'Defense Against the Dark Arts',
+        choice: 'A History of Magic',
+        choice: 'Astronomy',
+    },
+    correctAnswer: 3,
+},
+{
+    question: "Who is the cousin of Sirius Black?",
+    answers: {
+        choice: 'Voldemort',
+        choice: 'Bellatrix Lestrange',
+        choice: 'Lucius Malfoy',
+        choice: 'Filius Flitwick',
+    },
+    correctAnswer: 2,
+},
+{
+    question: "Who teaches Defence Against the Dark Arts in Order of the Phoenix?",
+    answers: {
+        choice: 'Remus Lupin',
+        choice: 'Mad Eye Moody',
+        choice: 'Severus Snape',
+        choice: 'Dolores Umbridge',
+    },
+    correctAnswer: 4,
+},
+{
+    question: "How many Horcruxes does Voldemort create?",
+    answers: {
+        choice: '5',
+        choice: '6',
+        choice: '7',
+        choice: '8',
+    },
+    correctAnswer: 3,
+},
+{
+    question: "Who owned Hepzibah Smiths cup with the badger on it?",
+    answers: {
+        choice: 'Helga Hufflepuff',
+        choice: 'Borgin and Burke',
+        choice: 'Grindewald',
+        choice: 'Cornelius Fudge',
+    },
+    correctAnswer: 1,
+},
+{
+    question: "Who discovers the original locket Horcrux?",
+    answers: {
+        choice: 'Albus Dumbledore',
+        choice: 'Andromeda Tonks',
+        choice: 'Regulus Black',
+        choice: 'Tom Riddle',
+    },
+    correctAnswer: 3,
+},
+{
+    question: "Who killed Nagini?",
+    answers: {
+        choice: 'Harry Potter',
+        choice: 'Neville Longbottom',
+        choice: 'Ginny Weasley',
+        choice: 'Hermione Granger',
+    },
+    correctAnswer: 2,
+},
+{
+    question: "What is the name of Remus Lupin's son?",
+    answers: {
+        choice: 'Albus',
+        choice: 'Scorpius',
+        choice: 'Hugo',
+        choice: 'Teddy',
+    },
+    correctAnswer: 4,
+},
+];
